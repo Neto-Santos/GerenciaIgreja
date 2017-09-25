04bedc02c676be65bf056839092144cfaa8b1019
Neto-Santos <netos0223@gmail.com>
2017-09-24 23:01:14 -0300

    Update CadFuncaoMinisterialController.cs

--- Controllers/Cadastro/CadFuncaoMinisterialController.cs
+++ Controllers/Cadastro/CadFuncaoMinisterialController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,11 +10,11 @@ using GerenciaIgreja.Contexto;
 
 namespace GerenciaIgreja.Controllers.Cadastro
 {
-    public class CadFuncaoMinisterialController : Controller
+    public class FuncaoMinisterialController : Controller
     {
         appFuncaoMinisterial aplicacaoFuncMinisterial = new appFuncaoMinisterial();
         public ActionResult Index()
-        { 
+        {
             return View(aplicacaoFuncMinisterial.Listar());
         }
 
@@ -22,5 +22,46 @@ namespace GerenciaIgreja.Controllers.Cadastro
         {
             return Json(aplicacaoFuncMinisterial.Recuperar(Id));
         }
+
+        public JsonResult Inserir(FuncaoMinisterial funcaoMinisterial)
+        {
+            var resultado = "OK";
+            var mensagens = new List<string>();
+            var idSalvo = string.Empty;
+
+            if (!ModelState.IsValid)
+            {
+                resultado = "AVISO";
+                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    var id = aplicacaoFuncMinisterial.Inserir(funcaoMinisterial);
+
+                    if (id > 0)
+                    {
+                        idSalvo = id.ToString();
+                    }
+                    else
+                    {
+                        resultado = "ERRO";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    resultado = "ERRO";
+                }
+            }
+
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
+        }
+
+        public JsonResult Excluir(int Id)
+        {
+            return Json(aplicacaoFuncMinisterial.Excluir(Id));
+        }
     }
-}
+
+    }
