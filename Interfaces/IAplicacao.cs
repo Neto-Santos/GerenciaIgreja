6ef390548116136ece82256ab22aaa36b78e34ef
Neto-Santos <netos0223@gmail.com>
2017-09-25 00:00:57 -0300

    Add files via upload

---
+++ Interfaces/IAplicacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GerenciaIgreja.Models;
+using GerenciaIgreja.Aplicacao;
+using System.Threading.Tasks;
+
+namespace GerenciaIgreja
+{
+    interface IAplicacao
+    {
+        int Inserir(object objeto);
+        void Alterar(object objeto);
+        IEnumerable<object> Listar();
+        object Recuperar(int Id);
+        bool Excluir(int Id);
+        object RetornaUltimo();
+        
+    }
+}
