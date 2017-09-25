e3ebe761b4f0780e009bad4aba27a39e93e98f82
Neto Santos <netos0223@gmail.com>
2017-09-25 20:44:15 -0300

    Add files via upload

--- Aplicacao/appFuncaoMinisterial.cs
+++ Aplicacao/appFuncaoMinisterial.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,15 +20,6 @@ namespace GerenciaIgreja.Aplicacao
         public object Recuperar(int Id)
         {
             return contexto.FuncaoMinisterial.Where(x => x.Id == Id).First();
-        }       
-
-        public void Alterar(object funcaoMinisterial)
-        {
-            var modelo = (FuncaoMinisterial)funcaoMinisterial;
-            var alterar = contexto.FuncaoMinisterial.Where(x => x.Id == modelo.Id).First();
-            alterar.Nome = modelo.Nome;
-            alterar.Ativo = modelo.Ativo;
-            contexto.SaveChanges();
         }
 
         public object RetornaUltimo()
@@ -41,9 +32,16 @@ namespace GerenciaIgreja.Aplicacao
             var Id = 0;
             var objFuncaoMinisterial = (FuncaoMinisterial)objeto;
 
-            contexto.FuncaoMinisterial.Add(objFuncaoMinisterial);
-            contexto.SaveChanges();
-            Id = objFuncaoMinisterial.Id;
+            if (objFuncaoMinisterial.Id == 0)
+            {
+                contexto.FuncaoMinisterial.Add(objFuncaoMinisterial);
+                contexto.SaveChanges();
+                Id = objFuncaoMinisterial.Id;
+            }
+            else
+            {
+               Id =  Alterar(objFuncaoMinisterial);
+            }
 
             return Id;
 
@@ -67,6 +65,30 @@ namespace GerenciaIgreja.Aplicacao
             }
 
             return ret;
+        }
+
+        public IEnumerable<object> paginacao(int paginaAtual, int itensPagina)
+        {
+
+            return contexto.FuncaoMinisterial.OrderBy(x => x.Id)
+                       .Skip((paginaAtual - 1) * itensPagina)
+                       .Take(itensPagina);
+        }
+
+        public int RecuperaQuantidade()
+        {
+            return contexto.FuncaoMinisterial.Count();
+        }
+
+        public int Alterar(object objeto)
+        {
+            var modelo = (FuncaoMinisterial)objeto;
+            var alterar = contexto.FuncaoMinisterial.Where(x => x.Id == modelo.Id).First();
+            alterar.Nome = modelo.Nome;
+            alterar.Ativo = modelo.Ativo;
+            contexto.SaveChanges();
+
+            return modelo.Id;
         }
     }
 }
